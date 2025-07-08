373f8193b3393299c07ab581331d05463df531d4
diegomac <dijavaji@gmail.com>
2025-07-07 19:44:27 -0500

    primer commit

---
+++ src/test/java/ec/com/technoloqie/enrichmentAI/api/TecEnrichmentAiWsApplicationTests.java
@@ -0,0 +1,13 @@
+package ec.com.technoloqie.enrichmentAI.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TecEnrichmentAiWsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
